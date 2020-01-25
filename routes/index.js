9c60456b72b8fdde951b09b2b2c87460f6d37b66
Aruna <bethalam.aruna@gmail.com>
2020-01-25 12:23:17 +0530

    slot deployment

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var model={
+    title: 'Azure',
+    mesage: process.env.MESSAGE || "This is development"
+  }
+  res.render('index', model);
 });
 
 module.exports = router;
